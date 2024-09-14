84a137f72dfeac960cb84f8974cd6f9a0e9eb3fa
CarrieForle <53133715+CarrieForle@users.noreply.github.com>
2024-09-14 16:33:31 +0800

    drafting thread pool

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use std::{
     net::{TcpListener, TcpStream},
 };
 
-use WebServer::ThreadPool;
+use webserver::ThreadPool;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
