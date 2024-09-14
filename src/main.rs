3b5fa273441e780be85c65b2fcd8c9908affcb86
CarrieForle <53133715+CarrieForle@users.noreply.github.com>
2024-09-14 21:57:34 +0800

    limited requests to 10 before shutting down the server

--- src/main.rs
+++ src/main.rs
@@ -16,11 +16,13 @@ fn main() {
 
     let pool = ThreadPool::build(4).expect("Failed to initialize a thread pool of 4");
 
-    for stream in listener.incoming() {
+    for stream in listener.incoming().take(10) {
         let stream = stream.unwrap();
 
         pool.execute(|| handle_connection(stream));
     }
+
+    println!("Shutting down");
 }
 
 fn handle_connection(mut stream: TcpStream) {
@@ -31,7 +33,7 @@ fn handle_connection(mut stream: TcpStream) {
     //     .map(|result| result.unwrap())
     //     .take_while(|line| !line.is_empty())
     //     .collect();
-    let request_line = buf_reader.lines().next().unwrap().unwrap();
+    let request_line = buf_reader.lines().next().expect("Failed to iterate to the next request").expect("Failed to read request");
 
     let (status_line, filename) = match &request_line[..] {
         "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
@@ -42,10 +44,10 @@ fn handle_connection(mut stream: TcpStream) {
         _ => ("HTTP/1.1 404 Not Found", "404.html"),
     };
     
-    let contents = fs::read_to_string(filename).unwrap();
+    let contents = fs::read_to_string(filename).expect("Failed to read \"{filename}\"");
     let length = contents.len();
 
     let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
     
-    stream.write_all(response.as_bytes()).unwrap();
+    stream.write_all(response.as_bytes()).expect("Failed to write response");
 }
